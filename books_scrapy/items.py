4c00789092fe83d01a202dabb0ef55c742d0faa2
Junfeng Zhang <jeffzx15o@gmail.com>
2021-03-03 18:18:25 +0800

    Add Manga model.

--- books_scrapy/items.py
+++ books_scrapy/items.py
@@ -6,7 +6,17 @@
 import scrapy
 
 
-class BooksScrapyItem(scrapy.Item):
+class Manga(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    image_url = scrapy.Field()
+    status = scrapy.Field()
+    recently_updated = scrapy.Field()
+    authors = scrapy.Field()
+    categories = scrapy.Field()
+    excerpt = scrapy.Field()
+    chapters = scrapy.Field()
+    
+class Chapter(scrapy.Item):
+    name = scrapy.Field()
+    image_urls = scrapy.Field()
