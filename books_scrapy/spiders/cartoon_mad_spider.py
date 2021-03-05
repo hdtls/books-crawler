d5754b1265b58368a0fcb6210517b78a038befa4
Junfeng Zhang <dev@akii.me>
2021-03-05 13:31:04 +0800

    Improve CartoonMadSpider performance.

--- books_scrapy/spiders/cartoon_mad_spider.py
+++ books_scrapy/spiders/cartoon_mad_spider.py
@@ -1,91 +1,77 @@
 import scrapy
 import os
 import re
-import logging
 from books_scrapy.items import CartoonMadManga
 
-
 class CartoonMadSpider(scrapy.Spider):
-    name = 'cartoonmad'
-    allowed_domains = ['cartoonmad.com']
-    download_folder = 'cartoonmad'
+    name = "cartoonmad"
+    allowed_domains = ["cartoonmad.com"]
+    base_url = "https://www.cartoonmad.com"
+    start_urls = []
 
     custom_settings = {
         'MEDIA_ALLOW_REDIRECTS': True,
     }
 
-    def start_requests(self):
-        manga_no = getattr(self, 'no', None)
-        urls = []
+    def __init__(self, **kwargs):
+        super(CartoonMadSpider, self).__init__(**kwargs)
+        
+        id_list = getattr(self, "list", None)
 
         # Make sure input is valid. 
-        if manga_no == None or manga_no.strip() == "":
+        if id_list is None or id_list.strip() == "":
             return
 
-        mangas = manga_no.split(',')
-        for manga in mangas:
-            url = 'https://www.cartoonmad.com/comic/' + manga + '.html'
+        for m_id in id_list.split(','):
+            url = self.base_url + "/comic/" + m_id + ".html"
             
-            if url not in urls:
-                urls.append(url)
-
-                yield scrapy.Request(url, self.parse)
+            # Ignore duplicate url.
+            if url not in self.start_urls:
+                self.start_urls.append(url)
 
     def parse(self, response):
-        # 漫画id
-        manga_no = response.url.split('/')[-1].split('.')[0]
-        # 名称含有中文
-        manga_name = str(response.css('title::text').extract()[0][:-14].strip().replace('?', '').strip())
-        manga_save_folder = os.path.join(self.download_folder, manga_no + '_' + manga_name)
+        m_id = response.url.split("/")[-1].split(".")[0]
+        
+        # Handling missing chapters.
+        invalid_id_list = ["2500"]
+        if m_id in invalid_id_list:
+            return
 
-        # chapters = response.css("body > table > tr:nth-child(1) > td:nth-child(2) > table > tr:nth-child(4) > td > table > tr:nth-child(2) > td:nth-child(2) > table:nth-child(3) > tr > td a")
-        chapters = response.css("fieldset")[1].css("tr > td a")
+        # Format string using utf8 encoding.
+        m_name = str(response.css('title::text').get()[:-14].strip().replace('?', '').strip())
 
-        chapters_list = []
-        for chapter in chapters:
-            chapter_name = chapter.css('::text').extract()[0]
-            chapter_no = chapter_name.split(' ')[1]
-            chapters_list.append([chapter_name, chapter_no])
+        td_list = response.css("fieldset")[1].css("tr > td")
 
-        # 页数比较麻烦 selector 怎么取都会取多
-        # chapters_pages = response.css("body > table > tr:nth-child(1) > td:nth-child(2) > table > tr:nth-child(4) > td > table > tr:nth-child(2) > td:nth-child(2) > table:nth-child(3) > tr > td font::text")
-        chapters_pages = response.css("fieldset")[1].css("tr > td font::text")
-        # 每个章节的页数
-        chapters_pages_count = []
-        # 简单粗暴的处理
-        for x in range(len(chapters)):
-            page_count = chapters_pages[x].extract()[1:-2]
-            chapters_pages_count.append(page_count)
+        for index, td in enumerate(td_list):
+            c_name = td.css("a::text").get()
+            if c_name is None:
+                continue
+            c_id = c_name.split(" ")[1]
+            c_page_size = ""
+            
+            # Handing page fault.
+            if m_id == "1893":
+                if index == 0:
+                    c_page_size = "11"
+                elif index == 1:
+                    c_page_size = "21"
+            elif m_id == "3908":
+                if index == 0:
+                    c_page_size = "11"
+            else:        
+                c_page_size = td.css("font::text").get()[1:-2]
 
-        # 特殊处理
-        url_id = int(manga_no)
-        if url_id == 1893:
-            print('缺少页数特殊处理')
-            chapters_pages_count[0] = 11
-            chapters_pages_count[1] = 21
-        elif url_id == 2500:
-            print('缺少章节, 地址修正无效')
-            chapters_list = chapters_list[:5] + chapters_list[6:]
-            chapters_pages_count = chapters_pages_count[:5] + chapters_pages_count[6:]
-            print('chapters_list', chapters_list)
-        elif url_id == 3908:
-            print('缺少页数特殊处理')
-            chapters_pages_count[0] = 11
+            c_url = self.base_url + td.css("a::attr(href)").get()
 
-        # print('chapters', chapters)
-        for index, chapter in enumerate(chapters):
-            chapter_link = 'https://www.cartoonmad.com' + response.css("body > table > tr:nth-child(1) > td:nth-child(2) > table > tr:nth-child(4) > td > table > tr:nth-child(2) > td:nth-child(2) > table:nth-child(3) > tr > td a::attr(href)")[index].extract()
-            chapter_name = chapter.css('::text').extract()[0]
-            chapter_no = chapter_name.split(' ')[1]
-
-            check_path = 'download/' + manga_save_folder + '/' + chapter_name
-            if os.path.exists(check_path) and (int(chapters_pages_count[index]) == len(os.listdir(check_path))):
-                print('文件夹已存在, 文件都下载过了, 跳过')
+            img_dir_path = os.getcwd() + "/download/" + self.name + "/" + m_id + '_' + m_name + '/' + c_id
+            # Check whether file exists at `path` and file size equal to `c_page_size` to
+            # skip duplicate download operation. 
+            if os.path.exists(img_dir_path) and (int(c_page_size) == len(os.listdir(img_dir_path))):
                 continue
 
-            yield scrapy.Request(chapter_link, meta={'manga_no': manga_no, 'chapter_no': chapter_no, 'manga_name': manga_name, 'chapter_name': chapter_name, 'chapters_pages_count': chapters_pages_count, 'chapters_list': chapters_list, 'manga_save_folder': manga_save_folder, 'chapters_index': index}, callback=self.parse_page)
+            yield scrapy.Request(c_url, meta={'m_id': m_id, 'c_id': c_id, 'c_page_size': c_page_size, 'img_dir_path': img_dir_path}, callback=self.__page_parse)
 
-    def parse_page(self, response):
+    def __page_parse(self, response):
         """
         scrapy shell https://www.cartoonmad.com/comic/469500002025001.html
         scrapy shell https://www.cartoonmad.com/comic/169800012046001.html
@@ -93,97 +79,68 @@ class CartoonMadSpider(scrapy.Spider):
         scrapy shell https://www.cartoonmad.com/comic/872600012021001.html
         scrapy shell https://www.cartoonmad.cc/comic/870100022025003.html
         """
-
-        manga_no = response.meta['manga_no']
-        chapter_no = response.meta['chapter_no']
-        manga_name = response.meta['manga_name']
-        chapter_name = response.meta['chapter_name']
-        chapters_pages_count = response.meta['chapters_pages_count']
-        chapters_list = response.meta['chapters_list']
-        manga_save_folder = response.meta['manga_save_folder']
-        chapters_index = response.meta['chapters_index']
-        print('parse_page()', manga_no, manga_name)
-
-        # 跳过广告
+        # Hard code ad fixing.
         if '漫畫讀取中' in response.text:
-            print('是广告页...跳过')
-            pat = '''var link = '(.*?)';'''
-            res = re.search(pat, response.text)
-            chapter_link = res[1]
-            print('chapter_link', chapter_link)
-            yield scrapy.Request(chapter_link, meta={'manga_no': manga_no, 'chapter_no': chapter_no, 'manga_name': manga_name, 'chapter_name': chapter_name, 'chapters_pages_count': chapters_pages_count, 'chapters_list': chapters_list, 'manga_save_folder': manga_save_folder}, callback=self.parse_page)
+            pattern = '''var link = '(.*?)';'''
+            res = re.search(pattern, response.text)
+            c_url = res[1]
+            yield scrapy.Request(c_url, meta=response.meta, callback=self.__page_parse)
+            return
 
-        # image_url = response.css("img::attr(src)")[7].extract()
-        # print response.url
-        # if 'cartoonmad.com' not in image_url or '/image/panen.png' in image_url:
-        #     image_url = response.css("img::attr(src)")[6].extract()
-        print('response', response)
-        image_urls = response.css("img::attr(src)").extract()
-        print('image_urls', image_urls)
-        image_url = ''
+        urls = response.css("img::attr(src)").getall()
 
         # https://www.cartoonmad.com/comic/comicpic.asp?file=/4695/000/001
         # https://www.cartoonmad.com/home75378/4695/000/001.jpg
-        #
         # https://www.cartoonmad.com/comic/comicpic.asp?file=/3080/001/001&rimg=1
         # https://web3.cartoonmad.com/home13712/3080/001/001.jpg
-        image_url_prefix = ''
-        is_asp_request = False
-        has_suffix = False
-        for x in image_urls:
-            # new rule
-            if x in ['https://www.cartoonmad.com/image/rad1.gif', 'https://www.cartoonmad.com/image/panen.png', 'https://www.cartoonmad.com/image/rad.gif']:
-                continue
-            if 'cc.fun8.us' in x:
-                continue
-            if 'comicpic.asp' in x:
-                # print('是asp')
-                is_asp_request = True
-                if '&rimg=1' in x:
-                    has_suffix = True
-                break
-            elif 'cartoonmad' in x:
-                image_url = x
-                image_url_parts = image_url.split('/')
-                # print image_url
-                # print image_url_parts
-                image_url_prefix = image_url_parts[0] + '//' + image_url_parts[2] + '/' + image_url_parts[3] + '/'
-                break
 
-        # for index, chapter in enumerate(chapters_list):
-        index = chapters_index
-        chapter = chapters_list[index]
-        chapter_name = chapter[0]
-        chapter_no = chapter[1]
+        url_prefix = ''
+        url_suff = ""
 
+        # Placeholder image url list.
+        p_urls = [
+            'https://www.cartoonmad.com/image/rad1.gif',
+            'https://www.cartoonmad.com/image/panen.png',
+            'https://www.cartoonmad.com/image/rad.gif'
+        ]
+
+        for url in urls:
+            if url in p_urls:
+                # Skip invalid manga image url.
+                continue
+            if "cc.fun8.us" in url:
+                # Skip invalid manga image url.
+                continue
+            if "comicpic.asp" in url:
+                url_prefix = "https://www.cartoonmad.com/comic/comicpic.asp?file=/"
+                if '&rimg=1' in url:
+                    url_suff = '&rimg=1'
+                break
+            elif 'cartoonmad' in url:
+                url_splits = image_url.split('/')
+                url_prefix = url_splits[0] + '//' + url_splits[2] + '/' + url_splits[3] + '/'
+                url_suff = ".jpg"
+                break
         # 下载图片
         # https://web.cartoonmad.com/c37sn562e81/3899/001/010.jpg
         # https://www.cartoonmad.com/comic/comicpic.asp?file=/8726/001/002
 
-        item = CartoonMadManga()
-        item['imgfolder'] = manga_save_folder + '/' + chapter_name
-        # print('chapters_pages_count', chapters_pages_count)
-        for y in range(1, int(chapters_pages_count[index]) + 1):
-            if is_asp_request:
-                # print('是asp')
-                item['imgurl'] = 'https://www.cartoonmad.com/comic/comicpic.asp?file=/' + manga_no + '/' + chapter_no + '/' + str(y).zfill(3)
-                if has_suffix:
-                    item['imgurl'] += '&rimg=1'
-            else:
-                # print('不是asp')
-                item['imgurl'] = [image_url_prefix + manga_no + '/' + chapter_no + '/' + str(y).zfill(3) + '.jpg']
-            # print('download image: ', item['imgurl'])
-            item['imgname'] = str(y).zfill(3) + '.jpg'
-            img_file_path = item['imgfolder'] + '/' + item['imgname']
-            # skip files that already downloaded
-            # print('检测图片是否存在', img_file_path)
-            if os.path.exists('download/' + img_file_path):
-                print('skip', img_file_path)
+        manga = CartoonMadManga()
+        img_dir_path = response.meta["img_dir_path"]
+
+        for page in range(1, int(response.meta["c_page_size"]) + 1):    
+            manga['imgurl'] = url_prefix + response.meta["m_id"] + '/' + response.meta["c_id"] + '/' + str(page).zfill(3) + url_suff
+
+            manga['imgname'] = str(page).zfill(3) + '.jpg'
+            img_file_path = img_dir_path + '/' + manga['imgname']
+
+            # Skip files that already downloaded.
+            if os.path.exists(img_file_path):
                 continue
 
-            if not os.path.exists('download/' + item['imgfolder']):
-                print('创建目录')
-                os.makedirs(os.getcwd() + '/download/' + item['imgfolder'])
+            if not os.path.exists(img_dir_path):
+                os.makedirs(img_dir_path)
+
             headers = {
                 "Accept": "image/webp,image/apng,image/*,*/*;q=0.8",
                 "Accept-Encoding": "gzip, deflate, br",
@@ -194,6 +151,5 @@ class CartoonMadSpider(scrapy.Spider):
                 "Referer": response.url,
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
             }
-            item['imgheaders'] = headers
-            print('图片下载地址', item['imgurl'])
-            yield item
+            manga['imgheaders'] = headers
+            # yield manga
