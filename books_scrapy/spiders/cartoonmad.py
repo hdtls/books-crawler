aab880d0d912937a6e3cd5653ee2aecc1206554c
Junfeng Zhang <dev@akii.me>
2021-03-06 20:21:50 +0800

    Lint file with black.

--- books_scrapy/spiders/cartoonmad.py
+++ books_scrapy/spiders/cartoonmad.py
@@ -5,45 +5,46 @@ from books_scrapy.items import Manga
 from books_scrapy.items import MangaChapter
 from books_scrapy.items import Image
 
+
 class CartoonMadSpider(scrapy.Spider):
     name = "cartoonmad"
     allowed_domains = ["cartoonmad.com"]
     base_url = "https://www.cartoonmad.com"
     start_urls = []
 
-    custom_settings = {
-        'MEDIA_ALLOW_REDIRECTS': True,
-    }
+    custom_settings = {"MEDIA_ALLOW_REDIRECTS": True}
 
     def __init__(self, **kwargs):
         super(CartoonMadSpider, self).__init__(**kwargs)
-        
+
         id_list = getattr(self, "list", None)
 
-        # Make sure input is valid. 
+        # Make sure input is valid.
         if id_list is None or id_list.strip() == "":
             return
 
-        for m_id in id_list.split(','):
+        for m_id in id_list.split(","):
             url = self.base_url + "/comic/" + m_id + ".html"
-            
+
             # Ignore duplicate url.
             if url not in self.start_urls:
                 self.start_urls.append(url)
 
     def parse(self, response):
         m_id = response.url.split("/")[-1].split(".")[0]
-        
+
         # Handling missing chapters.
         invalid_id_list = ["2500"]
         if m_id in invalid_id_list:
             return
 
         # Format string using utf8 encoding.
-        m_name = str(response.css('title::text').get()[:-14].strip().replace('?', '').strip())
+        m_name = str(
+            response.css("title::text").get()[:-14].strip().replace("?", "").strip()
+        )
 
         # tr_list = response.css("td:nth-child(2) tr:nth-child(4) tr:nth-child(2)")
-      
+
         # m_cover_url = self.base_url + tr_list[1].css("img::attr(src)").get()
         # m_categories = tr_list.css("tr:nth-child(3) a::text").get()
         # m_authors = tr_list.css("tr:nth-child(5) td::text").get().strip()[6:].split(",")
@@ -57,7 +58,7 @@ class CartoonMadSpider(scrapy.Spider):
                 continue
             c_id = c_name.split(" ")[1]
             c_page_size = ""
-            
+
             # Handing page fault.
             if m_id == "1893":
                 if index == 0:
@@ -67,26 +68,48 @@ class CartoonMadSpider(scrapy.Spider):
             elif m_id == "3908":
                 if index == 0:
                     c_page_size = "11"
-            else:        
+            else:
                 c_page_size = td.css("font::text").get()[1:-2]
 
             c_url = self.base_url + td.css("a::attr(href)").get()
 
             # Get images store from settings.
             img_dir_path = self.settings["IMAGES_STORE"]
-            img_dir_path = img_dir_path + "/" + self.name + "/" + m_id + '_' + m_name + '/' + c_name
+            img_dir_path = (
+                img_dir_path
+                + "/"
+                + self.name
+                + "/"
+                + m_id
+                + "_"
+                + m_name
+                + "/"
+                + c_name
+            )
 
             # Check whether file exists at `path` and file size equal to `c_page_size` to
-            # skip duplicate download operation. 
-            if os.path.exists(img_dir_path) and (int(c_page_size) == len(os.listdir(img_dir_path))):
+            # skip duplicate download operation.
+            if os.path.exists(img_dir_path) and (
+                int(c_page_size) == len(os.listdir(img_dir_path))
+            ):
                 continue
 
-            yield scrapy.Request(c_url, meta={"m_id": m_id, "c_name": c_name, "c_id": c_id, "c_page_size": c_page_size, "img_dir_path": img_dir_path}, callback=self.__page_parse)
+            yield scrapy.Request(
+                c_url,
+                meta={
+                    "m_id": m_id,
+                    "c_name": c_name,
+                    "c_id": c_id,
+                    "c_page_size": c_page_size,
+                    "img_dir_path": img_dir_path,
+                },
+                callback=self.__page_parse,
+            )
 
     def __page_parse(self, response):
         # Hard code ad fixing.
-        if '漫畫讀取中' in response.text:
-            pattern = '''var link = '(.*?)';'''
+        if "漫畫讀取中" in response.text:
+            pattern = """var link = '(.*?)';"""
             res = re.search(pattern, response.text)
             c_url = res[1]
             yield scrapy.Request(c_url, meta=response.meta, callback=self.__page_parse)
@@ -99,14 +122,14 @@ class CartoonMadSpider(scrapy.Spider):
         # https://www.cartoonmad.com/comic/comicpic.asp?file=/3080/001/001&rimg=1
         # https://web3.cartoonmad.com/home13712/3080/001/001.jpg
 
-        url_prefix = ''
+        url_prefix = ""
         url_suff = ""
 
         # Placeholder image url list.
         p_urls = [
-            'https://www.cartoonmad.com/image/rad1.gif',
-            'https://www.cartoonmad.com/image/panen.png',
-            'https://www.cartoonmad.com/image/rad.gif'
+            "https://www.cartoonmad.com/image/rad1.gif",
+            "https://www.cartoonmad.com/image/panen.png",
+            "https://www.cartoonmad.com/image/rad.gif",
         ]
 
         for url in urls:
@@ -118,12 +141,14 @@ class CartoonMadSpider(scrapy.Spider):
                 continue
             if "comicpic.asp" in url:
                 url_prefix = "https://www.cartoonmad.com/comic/comicpic.asp?file=/"
-                if '&rimg=1' in url:
-                    url_suff = '&rimg=1'
+                if "&rimg=1" in url:
+                    url_suff = "&rimg=1"
                 break
-            elif 'cartoonmad' in url:
-                url_splits = image_url.split('/')
-                url_prefix = url_splits[0] + '//' + url_splits[2] + '/' + url_splits[3] + '/'
+            elif "cartoonmad" in url:
+                url_splits = image_url.split("/")
+                url_prefix = (
+                    url_splits[0] + "//" + url_splits[2] + "/" + url_splits[3] + "/"
+                )
                 url_suff = ".jpg"
                 break
 
@@ -150,10 +175,16 @@ class CartoonMadSpider(scrapy.Spider):
             }
 
             image = Image(
-                name = image_name + '.jpg',
-                file_path = response.meta["img_dir_path"] + '/' + image_name + '.jpg',
-                url = url_prefix + response.meta["m_id"] + '/' + response.meta["c_id"] + '/' + image_name + url_suff,
-                http_headers = http_headers
+                name=image_name + ".jpg",
+                file_path=response.meta["img_dir_path"] + "/" + image_name + ".jpg",
+                url=url_prefix
+                + response.meta["m_id"]
+                + "/"
+                + response.meta["c_id"]
+                + "/"
+                + image_name
+                + url_suff,
+                http_headers=http_headers,
             )
 
             image_list.append(image)
