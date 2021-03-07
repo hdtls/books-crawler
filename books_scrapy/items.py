938647c95d0efe242bbe5dc4a2fe31aded9437c6
Junfeng Zhang <dev@akii.me>
2021-03-08 07:17:04 +0800

    Fix typo and function call error.

--- books_scrapy/items.py
+++ books_scrapy/items.py
@@ -9,14 +9,14 @@ import scrapy
 class Manga(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
-    image_url = scrapy.Field()
+    cover_image = scrapy.Field()
     status = scrapy.Field()
-    recently_updated = scrapy.Field()
     authors = scrapy.Field()
     categories = scrapy.Field()
     excerpt = scrapy.Field()
     chapters = scrapy.Field()
-        
+
+
 class Image(scrapy.Item):
     name = scrapy.Field()
     file_path = scrapy.Field()
@@ -24,13 +24,16 @@ class Image(scrapy.Item):
     http_headers = scrapy.Field()
     size = scrapy.Field()
 
+
 class MangaChapter(scrapy.Item):
     name = scrapy.Field()
     images = scrapy.Field()
     image_urls = scrapy.Field()
+    parent_id = scrapy.Field()
+
 
 class CartoonMadManga(scrapy.Item):
-        # define the fields for your item here like:
+    # define the fields for your item here like:
     # name = scrapy.Field()
     image_urls = scrapy.Field()
 
