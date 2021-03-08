41c625377d3e74f8d37e4755f17c5d9ba494acb3
Junfeng Zhang <jeffzx15o@gmail.com>
2021-03-08 08:17:27 +0800

    Update private function name prefix.

--- books_scrapy/spiders/ohmanhua.py
+++ books_scrapy/spiders/ohmanhua.py
@@ -14,16 +14,21 @@ from scrapy import Request
 class OHManhuaSpider(scrapy.Spider):
     name = "ohmanhua"
     base_url = "https://www.cocomanhua.com"
-    start_urls = ["https://www.cocomanhua.com/18741"]
+    start_urls = ["https://www.cocomanhua.com/show?orderBy=update"]
 
     def start_requests(self):
         for url in self.start_urls:
-            yield Request(url, self.parse)
+            if "?" in url:
+                yield Request(url, self.parse)
+            elif url.endswith(".html"):
+                yield Request(url, self.parse_chapter_page)
+            elif url.endswith("/"):
+                yield Request(url, self.parse_detail_page)
 
     def parse(self, html):
-        return self.__parse_detail_page(html)
+        return self.parse_detail_page(html)
 
-    def __parse_detail_page(self, html):
+    def parse_detail_page(self, html):
         manga = Manga()
         manga["name"] = html.css("dd.fed-deta-content h1::text").get()
 
@@ -31,7 +36,7 @@ class OHManhuaSpider(scrapy.Spider):
         cover_image["name"] = "cover.jpg"
         cover_image["url"] = html.css("dt.fed-deta-images a::attr(data-original)").get()
 
-        cover_image["file_path"] = self.__get_img_store(manga["name"])
+        cover_image["file_path"] = self.get_img_store(manga["name"])
 
         manga["cover_image"] = cover_image
 
@@ -59,18 +64,16 @@ class OHManhuaSpider(scrapy.Spider):
         for li in html.css("div.all_data_list li"):
             name = str(li.css("a::text").get().strip())
 
-            img_store = self.__get_img_store(manga["name"], name)
+            img_store = self.get_img_store(manga["name"], name)
 
             # Skip download exists page.
             if Path(img_store).exists():
                 continue
 
             url = self.base_url + li.css("a::attr(href)").get()
-            yield Request(
-                url, self.__parse_chapter_page, meta={"ref_id": manga["name"]}
-            )
+            yield Request(url, self.parse_chapter_page, meta={"ref_id": manga["name"]})
 
-    def __get_img_store(self, name, chapter_name=None):
+    def get_img_store(self, name, chapter_name=None):
         fragments = [self.settings["IMAGES_STORE"], self.name]
         if name is not None:
             fragments.append(name)
@@ -80,29 +83,29 @@ class OHManhuaSpider(scrapy.Spider):
 
         return "/".join(fragments)
 
-    def __parse_chapter_page(self, html):
+    def parse_chapter_page(self, html):
         chapter_data = html.css('script:contains("var C_DATA")::text').get()[12:-2]
 
         if chapter_data is None:
             return None
 
-        chapter_data = self.__decode(chapter_data)
+        chapter_data = self._decode(chapter_data)
 
         if chapter_data is None:
             return None
 
         chapter = MangaChapter()
 
-        img_store = self.__get_img_store(
+        img_store = self.get_img_store(
             chapter_data["mhname"],
             chapter_data["pagename"],
         )
-        chapter["image_urls"] = self.__get_img_list(chapter_data, img_store)
+        chapter["image_urls"] = self._get_img_list(chapter_data, img_store)
 
         print(chapter)
         # yield chapter
 
-    def __get_img_list(self, chap, img_dir_path):
+    def _get_img_list(self, chap, img_dir_path):
         total_img_size = int(chap["total_img_size"])
         image_list = []
 
@@ -128,10 +131,10 @@ class OHManhuaSpider(scrapy.Spider):
 
         return image_list
 
-    def __decode(self, ciphertext):
+    def _decode(self, ciphertext):
         assert isinstance(ciphertext, str)
 
-        plaintext = self.__try_decrypt(ciphertext)
+        plaintext = self._try_decrypt(ciphertext)
 
         mh_key = "mh_info="
         img_key = "image_info="
@@ -174,17 +177,17 @@ class OHManhuaSpider(scrapy.Spider):
                 image_info = demjson.decode(va_list[1])
 
         if mh_info["enc_code1"]:
-            mh_info["total_img_size"] = self.__try_decrypt(mh_info["enc_code1"])
+            mh_info["total_img_size"] = self._try_decrypt(mh_info["enc_code1"])
 
         if mh_info["enc_code2"]:
-            mh_info["img_path"] = self.__try_decrypt(
+            mh_info["img_path"] = self._try_decrypt(
                 mh_info["enc_code2"],
                 "fw125gjdi9ertyui",
             )
 
         return mh_info
 
-    def __try_decrypt(self, ciphertext, key=None, default_key=None):
+    def _try_decrypt(self, ciphertext, key=None, default_key=None):
         ciphertext = base64.b64decode(ciphertext)
 
         # 2019/9/27: "JRUIFMVJDIWE569j"
@@ -199,14 +202,14 @@ class OHManhuaSpider(scrapy.Spider):
             key_list.insert(0, key)
 
         for k in key_list:
-            plaintext = self.__decrypt(ciphertext, k)
+            plaintext = self._decrypt(ciphertext, k)
 
             if plaintext is not None:
                 return plaintext
 
         return ciphertext
 
-    def __decrypt(self, ciphertext, key):
+    def _decrypt(self, ciphertext, key):
         if isinstance(key, str):
             key = key.encode("utf-8")
 
@@ -219,11 +222,11 @@ class OHManhuaSpider(scrapy.Spider):
 
         plaintext = decryptor.update(ciphertext) + decryptor.finalize()
 
-        plaintext = self.__pkcs7_unpadding(plaintext)
+        plaintext = self._pkcs7_unpadding(plaintext)
 
         # Convert bytes to utf8 string.
         return plaintext.decode()
 
-    def __pkcs7_unpadding(self, data):
+    def _pkcs7_unpadding(self, buffer):
         unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
-        return unpadder.update(data) + unpadder.finalize()
+        return unpadder.update(buffer) + unpadder.finalize()
