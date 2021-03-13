4f3cb3b74df8a99be80b0ac7b260242742860db4
Junfeng Zhang <jeffzx15o@gmail.com>
2021-03-13 17:56:57 +0800

    Add 517manhua chapter crawl logic.

--- books_scrapy/utils.py
+++ books_scrapy/utils.py
@@ -1,11 +1,28 @@
+import re
+import json
+
+
 def fmt_label(label):
     return label.strip() if isinstance(label, str) else ""
+
+def fmt_url_domain(domain):
+    if not isinstance(domain, str):
+        return None
+    return domain[:-1] if domain.endswith("/") else domain
+
+def fmt_url_path(path):
+    if not isinstance(path, str):
+        return None
+    return path if path.startswith("/") else path[1:]
+
 
 def fmt_meta(arg):
     return {"__meta_key": arg}
 
+
 def revert_fmt_meta(arg):
     return arg["__meta_key"]
+
 
 def get_img_store(settings, rt, mid, last=None):
     assert rt and mid
@@ -18,3 +35,10 @@ def get_img_store(settings, rt, mid, last=None):
         fragments.append(last)
 
     return "/".join(fragments)
+
+
+def eval_js_variable(label, text):
+    match = re.findall(r"var %s=(.*);" % (label), text)
+    if not match:
+        return None
+    return json.loads(match[0])
