e4ad7b017d7967070b5d5da5a285860af508dbad
Junfeng Zhang <dev@akii.me>
2021-03-16 15:12:23 +0800

    Import dataclasses to refactor items.

--- books_scrapy/items.py
+++ books_scrapy/items.py
@@ -5,35 +5,76 @@
 
 import scrapy
 
+from dataclasses import dataclass
+from typing import Optional
 
-class Manga(scrapy.Item):
+
+@dataclass
+class Image:
+    url: str
+    name: Optional[str] = None
+    file_path: Optional[str] = None
+    http_headers: Optional[dict] = None
+
+
+@dataclass
+class Manga:
     # define the fields for your item here like:
-    name = scrapy.Field()
-    alias = scrapy.Field()
-    cover_image = scrapy.Field()
-    background_image = scrapy.Field()
-    promo_image = scrapy.Field()
-    authors = scrapy.Field()
-    status = scrapy.Field()
-    categories = scrapy.Field()
-    excerpt = scrapy.Field()
-    area = scrapy.Field()
-    ref_url = scrapy.Field()
+    authors: list[str]
+    cover_image: Image
+    excerpt: str
+    name: str
+    ref_url: str
+    area: Optional[str] = None
+    alias: Optional[str] = None
+    background_image: Optional[Image] = None
+    promo_image: Optional[Image] = None
+    status: Optional[str] = None
+    categories: Optional[list[str]] = None
 
 
-class Image(scrapy.Item):
-    name = scrapy.Field()
-    file_path = scrapy.Field()
-    url = scrapy.Field()
-    http_headers = scrapy.Field()
-    size = scrapy.Field()
+@dataclass
+class MangaChapter:
+    name: str
+    ref_url: str
+    rel_m_id: str
+    rel_m_title: str
+    image_urls: list[Image]
+
+    @property
+    def page_size(self):
+        return len(self.image_urls)
 
 
-class MangaChapter(scrapy.Item):
-    name = scrapy.Field()
-    ref_url = scrapy.Field()
-    images = scrapy.Field()
-    image_urls = scrapy.Field()
-    page_size = scrapy.Field()
-    rel_m_id = scrapy.Field()
-    rel_m_title = scrapy.Field()
+@dataclass
+class QTcmsObject:
+    qTcms_Cur: Optional[str]
+    qTcms_S_m_id: Optional[str]
+    qTcms_S_p_id: Optional[str]
+    qTcms_S_m_name: Optional[str]
+    qTcms_S_classid1pinyin: Optional[str]
+    qTcms_S_titlepinyin: Optional[str]
+    qTcms_S_m_playm: Optional[str]
+    qTcms_S_m_mhttpurl: Optional[str]
+    qTcms_S_m_murl_e: Optional[str]
+    qTcms_S_m_murl_e2: Optional[str]
+    qTcms_S_m_murl_e3: Optional[str]
+    qTcms_Pic_nextArr: Optional[str]
+    qTcms_Pic_backArr: Optional[str]
+    qTcms_Pic_curUrl: Optional[str]
+    qTcms_Pic_nextUrl: Optional[str]
+    qTcms_Pic_nextUrl_Href: Optional[str]
+    qTcms_Pic_len: Optional[str]
+    qTcms_Pic_backUrl: Optional[str]
+    qTcms_Pic_backUrl_Href: Optional[str]
+    qTcms_Pic_Cur_m_id: Optional[str]
+    qTcms_Pic_m_if: Optional[str]
+    qTcms_Pic_m_status2: Optional[str]
+    qTcms_m_moban: Optional[str]
+    qTcms_m_indexurl: Optional[str]
+    qTcms_m_webname: Optional[str]
+    qTcms_m_weburl: Optional[str]
+    qTcms_m_playurl: Optional[str]
+    qTcms_m_url: Optional[str]
+    qTcms_S_show_1: Optional[str]
+    qTcms_S_ifpubu: Optional[str]
--- books_scrapy/spiders/517mh.py
+++ books_scrapy/spiders/517mh.py
@@ -13,72 +13,6 @@ from urllib.parse import urlparse
 from scrapy import Request
 
 
-class QTcmsObject:
-    def __init__(
-        self,
-        qTcms_Cur,
-        qTcms_S_m_id,
-        qTcms_S_p_id,
-        qTcms_S_m_name,
-        qTcms_S_classid1pinyin,
-        qTcms_S_titlepinyin,
-        qTcms_S_m_playm,
-        qTcms_S_m_mhttpurl,
-        qTcms_S_m_murl_e,
-        qTcms_S_m_murl_e2,
-        qTcms_S_m_murl_e3,
-        qTcms_Pic_nextArr,
-        qTcms_Pic_backArr,
-        qTcms_Pic_curUrl,
-        qTcms_Pic_nextUrl,
-        qTcms_Pic_nextUrl_Href,
-        qTcms_Pic_len,
-        qTcms_Pic_backUrl,
-        qTcms_Pic_backUrl_Href,
-        qTcms_Pic_Cur_m_id,
-        qTcms_Pic_m_if,
-        qTcms_Pic_m_status2,
-        qTcms_m_moban,
-        qTcms_m_indexurl,
-        qTcms_m_webname,
-        qTcms_m_weburl,
-        qTcms_m_playurl,
-        qTcms_m_url,
-        qTcms_S_show_1,
-        qTcms_S_ifpubu,
-    ):
-        self.qTcms_Cur = qTcms_Cur
-        self.qTcms_S_m_id = qTcms_S_m_id
-        self.qTcms_S_p_id = qTcms_S_p_id
-        self.qTcms_S_m_name = qTcms_S_m_name
-        self.qTcms_S_classid1pinyin = qTcms_S_classid1pinyin
-        self.qTcms_S_titlepinyin = qTcms_S_titlepinyin
-        self.qTcms_S_m_playm = qTcms_S_m_playm
-        self.qTcms_S_m_mhttpurl = qTcms_S_m_mhttpurl
-        self.qTcms_S_m_murl_e = qTcms_S_m_murl_e
-        self.qTcms_S_m_murl_e2 = qTcms_S_m_murl_e2
-        self.qTcms_S_m_murl_e3 = qTcms_S_m_murl_e3
-        self.qTcms_Pic_nextArr = qTcms_Pic_nextArr
-        self.qTcms_Pic_backArr = qTcms_Pic_backArr
-        self.qTcms_Pic_curUrl = qTcms_Pic_curUrl
-        self.qTcms_Pic_nextUrl = qTcms_Pic_nextUrl
-        self.qTcms_Pic_nextUrl_Href = qTcms_Pic_nextUrl_Href
-        self.qTcms_Pic_len = qTcms_Pic_len
-        self.qTcms_Pic_backUrl = qTcms_Pic_backUrl
-        self.qTcms_Pic_backUrl_Href = qTcms_Pic_backUrl_Href
-        self.qTcms_Pic_Cur_m_id = qTcms_Pic_Cur_m_id
-        self.qTcms_Pic_m_if = qTcms_Pic_m_if
-        self.qTcms_Pic_m_status2 = qTcms_Pic_m_status2
-        self.qTcms_m_moban = qTcms_m_moban
-        self.qTcms_m_indexurl = qTcms_m_indexurl
-        self.qTcms_m_webname = qTcms_m_webname
-        self.qTcms_m_weburl = qTcms_m_weburl
-        self.qTcms_m_playurl = qTcms_m_playurl
-        self.qTcms_m_url = qTcms_m_url
-        self.qTcms_S_show_1 = qTcms_S_show_1
-        self.qTcms_S_ifpubu = qTcms_S_ifpubu
-
-
 class The517MangaSpider(scrapy.Spider):
     name = "www.517manhua.com"
     img_base_url = None
@@ -90,7 +24,7 @@ class The517MangaSpider(scrapy.Spider):
 
     def parse(self, response):
         return self.parse_detail_data(response)
-    
+
     def parse_detail_data(self, response):
         book = self.get_book_item(response)
         yield book
@@ -100,40 +34,42 @@ class The517MangaSpider(scrapy.Spider):
         file_path = get_img_store(self.settings, self.name, book["name"])
         if Path(file_path).exists() and len(os.listdir(file_path)) >= len(book_catalog):
             return
-        
+
         for chapter in book_catalog:
             file_path = file_path + "/" + chapter["name"]
-            
+
             yield Request(chapter["ref_url"], self.parse_chapter_data)
-            
+
     def get_book_item(self, response):
         main = response.xpath("//div[contains(@class, 'mh-date-info')]")
 
         name = main.xpath(
             "./div[contains(@class, 'mh-date-info-name')]//a/text()"
         ).get()
-        
-        cover_image = Image(url=response.xpath("//div[contains(@class, 'mh-date-bgpic')]//img/@src").get())
 
-        excerpt = main.xpath(
-            "./div[contains(@class, 'work-introd')]//p/text()"
-        ).get()
+        cover_image = Image(
+            url=response.xpath(
+                "//div[contains(@class, 'mh-date-bgpic')]//img/@src"
+            ).get()
+        )
+
+        excerpt = main.xpath("./div[contains(@class, 'work-introd')]//p/text()").get()
 
         authors = main.xpath(
             "./p[contains(@class, 'works-info-tc')]//em/a/text()"
         ).get()
         authors = authors.split("/") if authors else None
-        
+
         status = main.xpath(
             "./p[contains(@class, 'works-info-tc')][position()=2]/span[last()]/em/text()"
         ).get()
 
         return Manga(
-            authors,
-            cover_image,
-            excerpt,
-            name,
-            response.url
+            authors=authors,
+            cover_image=cover_image,
+            excerpt=excerpt,
+            name=name,
+            ref_url=ref_url,
         )
 
     def get_book_catalog(self, response):
@@ -144,66 +80,70 @@ class The517MangaSpider(scrapy.Spider):
             ref_url = parser.netloc + fmt_url_path(
                 fmt_label(li.xpath("./a/@href").get())
             )
-            chapter = MangaChapter(name, ref_url, response.url, name)
+
+            chapter = MangaChapter(
+                name=name,
+                ref_url=ref_url,
+                rel_m_id=rel_m_id,
+                rel_m_title=rel_m_title,
+                image_urls=[],
+            )
+
             book_catalog.append(chapter)
         return book_catalog
 
     def parse_chapter_data(self, response):
-        html_script_tag = response.xpath(
+        script_tag = response.xpath(
             "//script[contains(text(), 'var qTcms_S_m_murl_e=')]"
         ).get()
 
-        qTcms_S_m_murl_e = eval_js_variable("qTcms_S_m_murl_e", html_script_tag)
+        qTcms_S_m_murl_e = eval_js_variable("qTcms_S_m_murl_e", script_tag)
 
         if not qTcms_S_m_murl_e:
             self.logger.error("无法解析章节...")
             return
 
         qTcms_obj = QTcmsObject(
-            qTcms_Cur=eval_js_variable("qTcms_Cur", html_script_tag),
-            qTcms_S_m_id=eval_js_variable("qTcms_S_m_id", html_script_tag),
-            qTcms_S_p_id=eval_js_variable("qTcms_S_p_id", html_script_tag),
-            qTcms_S_m_name=eval_js_variable("qTcms_S_m_name", html_script_tag),
+            qTcms_Cur=eval_js_variable("qTcms_Cur", script_tag),
+            qTcms_S_m_id=eval_js_variable("qTcms_S_m_id", script_tag),
+            qTcms_S_p_id=eval_js_variable("qTcms_S_p_id", script_tag),
+            qTcms_S_m_name=eval_js_variable("qTcms_S_m_name", script_tag),
             qTcms_S_classid1pinyin=eval_js_variable(
-                "qTcms_S_classid1pinyin", html_script_tag
+                "qTcms_S_classid1pinyin", script_tag
             ),
-            qTcms_S_titlepinyin=eval_js_variable(
-                "qTcms_S_titlepinyin", html_script_tag
-            ),
-            qTcms_S_m_playm=eval_js_variable("qTcms_S_m_playm", html_script_tag),
-            qTcms_S_m_mhttpurl=eval_js_variable("qTcms_S_m_mhttpurl", html_script_tag),
+            qTcms_S_titlepinyin=eval_js_variable("qTcms_S_titlepinyin", script_tag),
+            qTcms_S_m_playm=eval_js_variable("qTcms_S_m_playm", script_tag),
+            qTcms_S_m_mhttpurl=eval_js_variable("qTcms_S_m_mhttpurl", script_tag),
             qTcms_S_m_murl_e=qTcms_S_m_murl_e,
-            qTcms_S_m_murl_e2=eval_js_variable("qTcms_S_m_murl_e2", html_script_tag),
-            qTcms_S_m_murl_e3=eval_js_variable("qTcms_S_m_murl_e3", html_script_tag),
-            qTcms_Pic_nextArr=eval_js_variable("qTcms_Pic_nextArr", html_script_tag),
-            qTcms_Pic_backArr=eval_js_variable("qTcms_Pic_backArr", html_script_tag),
-            qTcms_Pic_curUrl=eval_js_variable("qTcms_Pic_curUrl", html_script_tag),
-            qTcms_Pic_nextUrl=eval_js_variable("qTcms_Pic_nextUrl", html_script_tag),
+            qTcms_S_m_murl_e2=eval_js_variable("qTcms_S_m_murl_e2", script_tag),
+            qTcms_S_m_murl_e3=eval_js_variable("qTcms_S_m_murl_e3", script_tag),
+            qTcms_Pic_nextArr=eval_js_variable("qTcms_Pic_nextArr", script_tag),
+            qTcms_Pic_backArr=eval_js_variable("qTcms_Pic_backArr", script_tag),
+            qTcms_Pic_curUrl=eval_js_variable("qTcms_Pic_curUrl", script_tag),
+            qTcms_Pic_nextUrl=eval_js_variable("qTcms_Pic_nextUrl", script_tag),
             qTcms_Pic_nextUrl_Href=eval_js_variable(
-                "qTcms_Pic_nextUrl_Href", html_script_tag
+                "qTcms_Pic_nextUrl_Href", script_tag
             ),
-            qTcms_Pic_len=eval_js_variable("qTcms_Pic_len", html_script_tag),
-            qTcms_Pic_backUrl=eval_js_variable("qTcms_Pic_backUrl", html_script_tag),
+            qTcms_Pic_len=eval_js_variable("qTcms_Pic_len", script_tag),
+            qTcms_Pic_backUrl=eval_js_variable("qTcms_Pic_backUrl", script_tag),
             qTcms_Pic_backUrl_Href=eval_js_variable(
-                "qTcms_Pic_backUrl_Href", html_script_tag
+                "qTcms_Pic_backUrl_Href", script_tag
             ),
-            qTcms_Pic_Cur_m_id=eval_js_variable("qTcms_Pic_Cur_m_id", html_script_tag),
-            qTcms_Pic_m_if=eval_js_variable("qTcms_Pic_m_if", html_script_tag),
-            qTcms_Pic_m_status2=eval_js_variable(
-                "qTcms_Pic_m_status2", html_script_tag
-            ),
-            qTcms_m_moban=eval_js_variable("qTcms_m_moban", html_script_tag),
-            qTcms_m_indexurl=eval_js_variable("qTcms_m_indexurl", html_script_tag),
-            qTcms_m_webname=eval_js_variable("qTcms_m_webname", html_script_tag),
+            qTcms_Pic_Cur_m_id=eval_js_variable("qTcms_Pic_Cur_m_id", script_tag),
+            qTcms_Pic_m_if=eval_js_variable("qTcms_Pic_m_if", script_tag),
+            qTcms_Pic_m_status2=eval_js_variable("qTcms_Pic_m_status2", script_tag),
+            qTcms_m_moban=eval_js_variable("qTcms_m_moban", script_tag),
+            qTcms_m_indexurl=eval_js_variable("qTcms_m_indexurl", script_tag),
+            qTcms_m_webname=eval_js_variable("qTcms_m_webname", script_tag),
             qTcms_m_weburl=fmt_url_domain(
-                eval_js_variable("qTcms_m_weburl", html_script_tag)
+                eval_js_variable("qTcms_m_weburl", script_tag)
             ),
             qTcms_m_playurl=fmt_url_path(
-                eval_js_variable("qTcms_m_playurl", html_script_tag)
+                eval_js_variable("qTcms_m_playurl", script_tag)
             ),
-            qTcms_m_url=fmt_url_path(eval_js_variable("qTcms_m_url", html_script_tag)),
-            qTcms_S_show_1=eval_js_variable("qTcms_S_show_1", html_script_tag),
-            qTcms_S_ifpubu=eval_js_variable("qTcms_S_ifpubu", html_script_tag),
+            qTcms_m_url=fmt_url_path(eval_js_variable("qTcms_m_url", script_tag)),
+            qTcms_S_show_1=eval_js_variable("qTcms_S_show_1", script_tag),
+            qTcms_S_ifpubu=eval_js_variable("qTcms_S_ifpubu", script_tag),
         )
 
         image_urls = []
@@ -227,19 +167,13 @@ class The517MangaSpider(scrapy.Spider):
         for index, orig_url in enumerate(orig_url_list):
             img_name = str(index).zfill(3) + ".jpg"
             img_url = self.parse_img_url(orig_url, qTcms_obj)
-            img = Image(
-                name=img_name,
-                url=img_url,
-                file_path=None,
-                http_headers=None,
-            )
+            img = Image(name=img_name, url=img_url)
             image_urls.append(img)
 
         chapter = MangaChapter(
             name=qTcms_obj.qTcms_S_m_playm,
             ref_url=response.url,
             image_urls=image_urls,
-            page_size=len(image_urls),
             rel_m_id=qTcms_obj.qTcms_m_weburl + qTcms_obj.qTcms_m_url,
             rel_m_title=qTcms_obj.qTcms_S_m_name,
         )
