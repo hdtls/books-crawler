1f73d4462a577fa44ea1b73bbaae065546336f7b
Junfeng Zhang <dev@akii.me>
2021-03-23 16:12:57 +0800

    Lint spider and update request meta.

--- books_scrapy/spiders/__init__.py
+++ books_scrapy/spiders/__init__.py
@@ -6,10 +6,8 @@
 import os
 import scrapy
 
-from books_scrapy.items import Manga
-from books_scrapy.items import MangaChapter
-from books_scrapy.utils import get_img_store
-from pathlib import Path
+from books_scrapy.utils import fmt_meta
+
 
 class Spider(scrapy.Spider):
     """Base class for book spiders."""
@@ -18,39 +16,63 @@ class Spider(scrapy.Spider):
         return self.parse_detail_data(response)
 
     def parse_detail_data(self, response):
-        """Parse detail page data e.g., book info and catalog."""
+        """Parse detail page data (e.g. book info and catalog)."""
 
         book_info = self.get_book_info(response)
         yield book_info
 
         book_catalog = self.get_book_catalog(response)
 
-        file_path = get_img_store(self.settings, self.name, book_info.name)
+        file_path = self.get_file_store(book_info.fingerprint)
 
-        if Path(file_path).exists() and len(os.listdir(file_path)) >= len(book_catalog):
+        if os.path.exists(file_path) and len(os.listdir(file_path)) >= len(
+            book_catalog
+        ):
             # File size in `file_path` is greater than or equal to catalog size.
             # all catalog already been crawled.
             return
 
-        for entry in book_catalog:
-            yield scrapy.Request(entry.ref_url, self.parse_chapter_data)
+        yield from response.follow_all(
+            book_catalog,
+            self.parse_chapter_data,
+            meta=fmt_meta(book_info.fingerprint),
+        )
 
-    def get_book_info(self, response) -> Manga:
+    def get_book_info(self, response):
         """
         Get book information from `response` object.
         - Parameter response: Source data.
         - Returns: Book info model.
         """
-        raise NotImplementedError(f'{self.__class__.__name__}.get_book_info is not implemented')
-    
-    def get_book_catalog(self, response) -> list[MangaChapter]:
+        raise NotImplementedError(
+            f"{self.__class__.__name__}.get_book_info is not implemented"
+        )
+
+    def get_book_catalog(self, response):
         """
         Get book catalog from `response` object.
         - Parameter response: Source data.
         - Retures: Book catalog
         """
-        raise NotImplementedError(f'{self.__class__.__name__}.get_book_catalog is not implemented')
+        raise NotImplementedError(
+            f"{self.__class__.__name__}.get_book_catalog is not implemented"
+        )
 
     def parse_chapter_data(self, response):
-        """Parse chapter page data. Override this function to provide parsing logic."""
-        raise NotImplementedError(f'{self.__class__.__name__}.parse_chapter_data is not implemented.')
+        """
+        Parse chapter page data. Override this function to provide parsing logic.
+        All response have meta which value is fingerprint of this book.
+        """
+        raise NotImplementedError(
+            f"{self.__class__.__name__}.parse_chapter_data is not implemented."
+        )
+
+    def get_file_store(self, rt, c=None):
+        fragments = [self.settings["IMAGES_STORE"]]
+
+        fragments.append(rt)
+
+        if c:
+            fragments.append(c)
+
+        return "/".join(fragments)
