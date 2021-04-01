35ba95167af5da7c871a7fa16d4c48ca1c5c5e03
Junfeng Zhang <dev@akii.me>
2021-04-01 19:21:36 +0800

    Add zombie author registration.

--- books_scrapy/items.py
+++ books_scrapy/items.py
@@ -3,9 +3,13 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+import enum
 import hashlib
 
-from datetime import datetime, timezone
+from datetime import datetime
+from os import name
+
+from sqlalchemy.sql.expression import false, null
 from books_scrapy.utils import list_extend
 from dataclasses import dataclass, field
 from typing import Optional, List
@@ -14,19 +18,48 @@ from sqlalchemy.orm.decl_api import registry
 from sqlalchemy.sql.schema import Column, ForeignKey, Table
 from sqlalchemy.sql.sqltypes import (
     BigInteger,
+    Boolean,
     DateTime,
+    Enum,
     Integer,
     JSON,
     String,
     Text,
 )
 
+
 mapper_registry = registry()
 
 
+class Level(enum.Enum):
+    ZOMBIE = "zombie"
+
+
 @dataclass
-class MangaCategory:
-    pass
+@mapper_registry.mapped
+class Author:
+
+    __table__ = Table(
+        "users",
+        mapper_registry.metadata,
+        Column("id", BigInteger, autoincrement=True, nullable=False, primary_key=True),
+        Column("name", String, nullable=False, unique=True),
+        # Column("level", Enum(Level), default=Level.ZOMBIE, nullable=False),
+        Column("created_at", DateTime, default=datetime.utcnow()),
+        Column(
+            "updated_at",
+            DateTime,
+            default=datetime.utcnow(),
+            onupdate=datetime.utcnow(),
+        ),
+    )
+
+    name: str
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Author):
+            return false
+        return self.name == o.name
 
 
 @dataclass
@@ -48,12 +81,12 @@ class MangaChapter(Chapter):
         Column("image_urls", JSON(none_as_null=True), nullable=False),
         Column("ref_urls", JSON(none_as_null=True)),
         Column("book_id", BigInteger, ForeignKey("manga.id")),
-        Column("created_at", DateTime, default=datetime.now(timezone.utc)),
+        Column("created_at", DateTime, default=datetime.utcnow()),
         Column(
             "updated_at",
             DateTime,
-            default=datetime.now(timezone.utc),
-            onupdate=datetime.now(timezone.utc),
+            default=datetime.utcnow(),
+            onupdate=datetime.utcnow(),
         ),
     )
 
@@ -78,12 +111,13 @@ class MangaChapter(Chapter):
             self.image_urls = other.image_urls
 
 
-# author_manga_siblings = Table(
-#     "author_manga_siblings",
-#     mapper_registry.metadata,
-#     Column("user_id", Integer, ForeignKey("users.id"), nullable=False, primary_key=True),
-#     Column("manga_id", Integer, ForeignKey("manga.id"), nullable=False, primary_key=True),
-# )
+author_published_manga = Table(
+    "users_authored_manga",
+    mapper_registry.metadata,
+    Column("id", BigInteger, autoincrement=True, nullable=False, primary_key=True),
+    Column("from", BigInteger, ForeignKey("users.id")),
+    Column("book_id", BigInteger, ForeignKey("manga.id")),
+)
 
 
 @dataclass
@@ -97,25 +131,28 @@ class Manga:
         Column("name", String(255), nullable=False),
         Column("excerpt", Text, nullable=False),
         Column("cover_image", JSON(none_as_null=True), nullable=False),
+        Column("copyrighted", Boolean, default=False, nullable=False),
         Column("schedule", Integer, nullable=False),
         Column("ref_urls", JSON(none_as_null=True)),
         Column("aliases", JSON(none_as_null=True)),
         Column("background_image", JSON(none_as_null=True)),
         Column("promo_image", JSON(none_as_null=True)),
         Column("area_id", ForeignKey("manga_areas.id")),
-        Column("created_at", DateTime, default=datetime.now(timezone.utc)),
+        Column("created_at", DateTime, default=datetime.utcnow()),
         Column(
             "updated_at",
             DateTime,
-            default=datetime.now(timezone.utc),
-            onupdate=datetime.now(timezone.utc),
+            default=datetime.utcnow(),
+            onupdate=datetime.utcnow(),
         ),
     )
 
     __mapper_args__ = {
         "properties": {
             "chapters": relationship("MangaChapter", backref="manga"),
-            # "authors": relationship("User", secondary=author_manga_siblings)
+            "authors": relationship(
+                "Author", secondary=author_published_manga, backref="manga"
+            ),
         }
     }
 
@@ -126,18 +163,21 @@ class Manga:
     # Schedule for manga publishing. there only have two value,
     # 0 for inprogress or 1 for finished.
     schedule: int
-    authors: List[str] = field(default_factory=list)
+    # Relationship property remove from init.
+    authors: List[Author] = field(init=False)
+    copyrighted: bool = field(default=False, init=False)
     ref_urls: Optional[List[str]] = None
-    area: Optional[str] = None
-    area_id: Optional[int] = None
+    area_id: Optional[int] = field(default=None, init=False, repr=False)
     aliases: Optional[List[str]] = None
     background_image: Optional[dict] = None
     promo_image: Optional[dict] = None
     categories: Optional[List[str]] = None
-    chapters: Optional[List[MangaChapter]] = field(default_factory=list)
+    chapters: Optional[List[MangaChapter]] = None
 
     def make_signature(self):
-        plaintext = self.name + "-" + ",".join(self.authors)
+        plaintext = (
+            self.name + "-" + ",".join(map(lambda author: author.name, self.authors))
+        )
         plaintext = plaintext.encode("utf-8")
         md5 = hashlib.md5()
         md5.update(plaintext)
@@ -167,12 +207,12 @@ class MangaArea:
         mapper_registry.metadata,
         Column("id", Integer, autoincrement=True, primary_key=True),
         Column("name", String(), nullable=False, unique=True),
-        Column("created_at", DateTime, default=datetime.now(timezone.utc)),
+        Column("created_at", DateTime, default=datetime.utcnow()),
         Column(
             "updated_at",
             DateTime,
-            default=datetime.now(timezone.utc),
-            onupdate=datetime.now(timezone.utc),
+            default=datetime.utcnow(),
+            onupdate=datetime.utcnow(),
         ),
     )
 
