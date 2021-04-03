e6215fee392a54ad22b1b064fc359bc7e6c3dc41
Junfeng Zhang <dev@akii.me>
2021-04-03 12:33:18 +0800

    Add MangaChapterLoader.

--- books_scrapy/loaders.py
+++ books_scrapy/loaders.py
@@ -1,10 +1,16 @@
-from books_scrapy.items import Author, Manga, MangaArea, MangaCategory
-from itemloaders.processors import Identity, MapCompose, TakeFirst
+from books_scrapy.items import Author, Manga, MangaArea, MangaCategory, MangaChapter
+from itemloaders.processors import Compose, Identity, MapCompose, TakeFirst
 from scrapy.loader import ItemLoader
 
 
-def _make_img(url):
-    return dict(url=url, ref_urls=[url]) if url else None
+def make_img(value):
+    if isinstance(value, list):
+        return [
+            dict(name=str(i).zfill(4) + ".jpg", url=url, ref_urls=[url])
+            for i, url in enumerate(value)
+        ]
+    else:
+        return None
 
 
 def split_with_space_or_comma(value):
@@ -19,20 +25,29 @@ def split_with_space_or_comma(value):
 
 
 class MangaLoader(ItemLoader):
-
     default_input_processor = MapCompose(str.strip)
     default_output_processor = TakeFirst()
+    default_item_class = Manga
 
-    cover_image_in = MapCompose(_make_img)
+    cover_image_in = Compose(make_img)
     schedule_in = MapCompose(lambda s: 1 if "完结" in s[0] else 0)
     authors_in = MapCompose(split_with_space_or_comma, lambda name: Author(name=name))
     authors_out = Identity()
     ref_urls_out = Identity()
     area_in = MapCompose(lambda name: MangaArea(name=name))
     aliases_in = MapCompose(split_with_space_or_comma)
-    background_image_in = MapCompose(_make_img)
-    promo_image_in = MapCompose(_make_img)
+    background_image_in = Compose(make_img)
+    promo_image_in = Compose(make_img)
     categories_in = MapCompose(
         split_with_space_or_comma, lambda name: MangaCategory(name=name)
     )
     categories_out = Identity()
+
+
+class MangaChapterLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+    default_item_class = MangaChapter
+
+    ref_urls_out = Identity()
+    image_urls_in = Compose(make_img)
+    image_urls_out = Identity()
