6b94ce277ab9cbb30d482447346c06bf509162f4
Junfeng Zhang <dev@akii.me>
2021-04-19 12:28:45 +0800

    Update file system store.

--- books_scrapy/pipelines/images.py
+++ books_scrapy/pipelines/images.py
@@ -1,12 +1,18 @@
 import hashlib
 import logging
-import scrapy
+import os
 
 from books_scrapy.utils import fmt_meta
 from books_scrapy.utils import revert_fmt_meta
 from itemadapter import ItemAdapter
 from pathlib import Path
 from scrapy.pipelines import images
+from scrapy.pipelines.files import (
+    FSFilesStore,
+    FTPFilesStore,
+    GCSFilesStore,
+    S3FilesStore,
+)
 from scrapy.utils.python import to_bytes
 from scrapy.utils.project import get_project_settings
 
@@ -14,7 +20,37 @@ from scrapy.utils.project import get_project_settings
 logger = logging.getLogger(__name__)
 
 
+class FSImageStore(FSFilesStore):
+    def stat_file(self, path, info):
+        absolute_path = self._get_filesystem_path(path)
+        try:
+            last_modified = os.path.getmtime(absolute_path)
+        except os.error:
+            return {}
+
+        with open(absolute_path, "rb") as f:
+            data = f.read()
+            checksum = hashlib.md5(data).hexdigest()
+            image = Image.open(BytesIO(data))
+
+        return {
+            "last_modified": last_modified,
+            "checksum": checksum,
+            "width": image.width,
+            "height": image.height,
+        }
+
+
 class ImagesPipeline(images.ImagesPipeline):
+    """Abstract pipeline that implement the image thumbnail generation logic"""
+
+    STORE_SCHEMES = {
+        "": FSFilesStore,
+        "file": FSImageStore,
+        "s3": S3FilesStore,
+        "gs": GCSFilesStore,
+        "ftp": FTPFilesStore,
+    }
 
     def get_media_requests(self, item, info):
         urls = ItemAdapter(item).get(self.images_urls_field, [])
