92d403a8fd2a63e30bc2aa76acc6355be8390b46
Junfeng Zhang <jeffzx15o@gmail.com>
2021-04-24 18:23:49 +0800

    Add bili id generate algorithm.

---
+++ books_scrapy/utils/bili.py
@@ -0,0 +1,8 @@
+def bili(id):
+    table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF"
+    indices = [11, 10, 3, 8, 4, 6]
+    x = (id ^ 177451812) + 8728348608
+    result = list("CM1  4 1 7  ")
+    for i in range(len(indices)):
+        result[indices[i]] = table[x // 58 ** i % 58]
+    return result
